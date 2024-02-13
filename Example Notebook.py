b5d531b8d682010424f43649c7f52c2e1b5b5cf0
lokesh-hindusthan <lokesh-hindusthan>
2024-02-13 15:15:48 +0000

    add

--- Example Notebook.py
+++ Example Notebook.py
@@ -6,6 +6,7 @@
 # COMMAND ----------
 
 data = [[1, "Elia"], [2, "Teo"], [3, "Fang"]]
+data = [[1, "Elia"], [2, "Teo"], [3, "Fang"]]
 sdf = spark.createDataFrame(data, schema="id LONG, name STRING")
 display(sdf)
 
